61981fb1d0668df6ae7cb9174f569af7b4b13382
tangkaiqiang <justforgit@163.com>
2018-06-07 16:49:47 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ export default {
     // 调用API从本地缓存中获取数据
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // wx.setStorageSync('logs', logs)
 
     console.log('app created and cache logs by setStorageSync')
   }
